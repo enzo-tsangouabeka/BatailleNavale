7ff60b970129e122ff9cf60e632dc5986ff56927
Enzo <enzo.t@lycee-de-beauvoir.education>
2024-11-19 17:41:59 +0100

    Création du projet

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file main.cpp
+ * @brief Projet de Bataille navale
+ * @author Enzo Tsangouabeka
+ * @version v1.0
+ * @date 9/11/2024
+ */
+
+/* CONSIGNE :
+
+    → 2 joueurs
+    → 1 plateau à 2 tableaux
+
+Tour à tour, les joueurs doivent trouver les positions des navires de l'autre joueur pour les couler.
+La partie est terminée quand tout les navires de l'adversaire sont coulés. Pour simplifier, un plateau de 10x10 cases pourra suffir avec dans ce cas 1 bateau de 4, un autre de 3 et un bateau de 2.
+
+Niveau 0:
+Votre programme simule une partie en affichant le plateau de chaque joueur à tour de role sous la forme d'un tableau 2D. Les cases sont choisies par les deux joueurs et la fin de partie déterminée visuellement. Les navires sont positionnés dès le départ par le programme et ne peuvent être modifiés par les joueurs.
+
+Niveau 1:
+Les navires sont placés par les joueurs.
+
+Niveau 2:
+Un joueur joue contre l'ordinateur qui choisit aléatoirement ses tirs.
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
